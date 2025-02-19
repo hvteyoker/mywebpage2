5a810f973d6ab3ca4f3469f82badf1cdc69244bb
hvteyoker <144468459+hvteyoker@users.noreply.github.com>
2025-02-19 18:13:55 +0400

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,7 +31,7 @@ const Index = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="background.mp4" type="video/mp4" />
+        <source src="/background.mp4" type="video/mp4" />
         Ваш браузер не поддерживает видео-тег.
       </video>
 
